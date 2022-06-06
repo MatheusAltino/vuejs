495abd97b1ac8c79fba5671281777f6ca63c95eb
Matheus Altino Pantoja Mendonça <maltino@supermed.com.br>
2022-06-06 17:04:07 -0300

    :sparkles: Add mataMonstros

---
+++ projeto-01-monstro/app.js
@@ -0,0 +1,14 @@
+export default new Vue({
+    el: '#app',
+    data: {
+        playerLife: 100,
+        monsterLife: 100
+    },
+    methods: {
+
+    },
+    watch: {
+         
+    }
+   
+})
